90a3186940f6671da5042f7a8e70cafde88f7b98
rebecarivas <rebecarivas@gmail.com>
2022-01-26 23:06:02 -0300

    feature: adiciona o componente table no dashboard

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,10 +1,12 @@
 import Summary from "../Summary";
+import Table from "../Table";
 import { Container } from "./style"
 
 const Dashboard: React.FC = () => {
     return(
         <Container>
             <Summary />
+            <Table />
         </Container>
     )
 }
